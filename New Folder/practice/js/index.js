6b2a869b01171efdc98e8081428122c476ace9a8
HexX <118554154+painXyuri@users.noreply.github.com>
2024-11-27 09:48:41 -0600

    Delete New Folder directory

--- New Folder/practice/js/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("project3").onclick = displayDate
-document.getElementById("project2")
-document.getElementById("project1")
-
-function displayDate(){
-    document.getElementById("demo").innerHTML = displayDate()
-}
