b2a1112c2332840cb47bb1aa8aba204b722161b7
HexX <118554154+painXyuri@users.noreply.github.com>
2024-11-27 09:43:29 -0600

    Add files via upload

---
+++ New Folder/practice/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("project3").onclick = displayDate
+document.getElementById("project2")
+document.getElementById("project1")
+
+function displayDate(){
+    document.getElementById("demo").innerHTML = displayDate()
+}
